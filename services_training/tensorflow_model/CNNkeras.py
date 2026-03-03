fb325fb5448efac2238709f68d471b76f0b8ccf7
Gabiot Victor <victorgabiot@gmail.com>
2026-03-03 11:57:59 +0100

    ajout des requirements et déplacement dans les contenaires des modèles

---
+++ services_training/tensorflow_model/CNNkeras.py
@@ -0,0 +1,107 @@
+import os
+# Suppress TensorFlow logging messages because we don't GPU on our machine #CYTech
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+from tensorflow.keras import layers
+import tensorflow as tf
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.datasets import cifar100
+import time
+import psutil
+
+
+# ========================
+# Custom callback
+# ========================
+class LiveMetricsCallback(tf.keras.callbacks.Callback):
+    
+    def on_train_begin(self, logs=None):
+        self.last_print_time = time.time()
+        self.process = psutil.Process(os.getpid())
+    
+    def on_batch_end(self, batch, logs=None):
+        current_time = time.time()
+        
+        # Print every 5 seconds
+        if current_time - self.last_print_time >= 5:
+            cpu_count = psutil.cpu_count()
+            cpu_usage = self.process.cpu_percent()/cpu_count
+            # RAM utilisée par le process Python (en MB)
+            ram_usage = self.process.memory_info().rss / 1024**2
+            
+            # RAM totale utilisée sur la machine
+            system_ram = psutil.virtual_memory().percent
+            
+            print(f"\n[Live Metrics]")
+            print(f"Loss: {logs['loss']:.4f}")
+            print(f"Accuracy: {logs.get('accuracy', 0):.4f}")
+            print(f"CPU Usage: {cpu_usage}%")
+            print(f"Process RAM: {ram_usage:.2f} MB")
+            print(f"System RAM Usage: {system_ram}%")
+            
+            self.last_print_time = current_time
+
+
+def train_model():
+    #download the dataset and split it into training and test sets
+    image_size = (32, 32, 3)
+    nb_classes = 100
+
+
+    (x_train, y_train), (x_test, y_test) = cifar100.load_data()
+    assert x_train.shape == (50000, 32, 32, 3)
+    assert x_test.shape == (10000, 32, 32, 3)
+    assert y_train.shape == (50000, 1)
+    assert y_test.shape == (10000, 1)
+
+    # Normalize
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+
+    #data augmentation of cifar100 dataset because only 600 image per class and we have 100 classes, so we need to augment the data to increase the number of images per class and improve the performance of the model
+    data_augmentation = Sequential(
+        [
+            layers.Normalization(),
+            layers.Resizing(image_size, image_size),
+            layers.RandomFlip("horizontal"),
+            layers.RandomRotation(factor=0.02),
+            layers.RandomZoom(
+                height_factor=0.2, width_factor=0.2
+            ),
+        ],
+        name="data_augmentation",
+    )
+    data_augmentation.layers[0].adapt(x_train)
+
+
+
+    model = Sequential([
+        layers.Conv2D(32, (3,3), activation='relu', input_shape=image_size),
+        layers.MaxPooling2D((2,2)),
+        
+        layers.Conv2D(64, (3,3), activation='relu'),
+        layers.MaxPooling2D((2,2)),
+        
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(nb_classes)  # 100 classes in CIFAR-100 dataset
+    ])
+
+
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+
+    # ========================
+    # Train with callback
+    # ========================
+    model.fit(
+        x_train,
+        y_train,
+        epochs=50,
+        batch_size=64,
+        callbacks=[LiveMetricsCallback()]
+    )
