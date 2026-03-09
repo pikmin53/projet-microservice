a5bafbb93de4774542c25f836651ea18854ed416
Waoutch <ma3leve@gmail.com>
2026-03-09 18:00:28 +0100

    add : lien kafka api sur le topic metrics_pytorch

--- api/src/routers/models.py
+++ api/src/routers/models.py
@@ -1,13 +1,20 @@
 from fastapi import APIRouter, Depends
 from src.core.security import verify_token
 from fastapi import Request
+from src.core.metrics import create_consumer, consume_metrics
 
 router = APIRouter()
+
+consumer = create_consumer()
 
 @router.get("/data")
 async def get_data(request : Request):
     value = verify_token(request)
     if value is None:
         return {"message": "Invalid token"}
+    else:
+        metrics = consume_metrics(consumer)
+        if metrics is not None:
+            print(f"Metrics reçues : {metrics}")
                 
-    return {"message": "Here is your data", "user": value}
+    return {"message": metrics, "user": value}
