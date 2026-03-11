216e725b2be60a03f57f8e89085cec8b9a55751d
Waoutch <ma3leve@gmail.com>
2026-03-11 10:23:46 +0100

    feat : lien entre les données à le front

--- api/src/routers/models.py
+++ api/src/routers/models.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from src.core.security import verify_token
-from fastapi import Request
 from src.core.metrics import create_consumer, consume_metrics
 
 router = APIRouter()
 
-consumer = create_consumer()
+consumer_pytorch = create_consumer("metrics_pytorch")
+consumer_tensorflow = create_consumer("metrics_tensorflow")
 
 @router.get("/data")
 async def get_data(request : Request):
@@ -13,7 +13,15 @@ async def get_data(request : Request):
     if value is None:
         return {"message": "Invalid token"}
     else:
-        metrics = consume_metrics(consumer)
+        metrics_pytorch = consume_metrics(consumer_pytorch)
+        metrics_tensorflow = consume_metrics(consumer_tensorflow)
+        if metrics_pytorch is None or metrics_tensorflow is None:
+            return {"message": {}, "user": value}
+        metrics = {
+            key: 
+                {"time": metrics_pytorch["time"], "pytorch": metrics_pytorch[key], "tensorflow": metrics_tensorflow[key]}
+            for key in metrics_pytorch if key != "time"
+        }
         if metrics is not None:
             print(f"Metrics reçues : {metrics}")
                 
