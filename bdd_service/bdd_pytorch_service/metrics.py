565b136362ba26bd450b1f0d5f46974a5733f8de
MarionM64 <marion.marty@cy-tech.fr>
2026-03-11 21:34:20 +0100

    fix bdd

--- bdd_service/bdd_pytorch_service/metrics.py
+++ bdd_service/bdd_pytorch_service/metrics.py
@@ -33,7 +33,7 @@ class MetricsPytorch(Base):
     cpu = Column(Float, nullable=False)
     ram = Column(Float, nullable=False)
     accuracy = Column(Float, nullable=False)
-    duration = Column(VARCHAR(100), nullable=False)
+    vitesse_exec = Column(Float, nullable=False)
     time = Column(DateTime, nullable=False)
 
 
@@ -47,7 +47,7 @@ def add_metrics(metrics: json):
         cpu=metrics["cpu"],
         ram=metrics["ram"],
         accuracy=metrics["accuracy"],
-        duration=metrics["duration"],
+        vitesse_exec=metrics["vitesse_exec"],
         time=metrics["time"])
     db.add(new_metrics)
     db.commit()
